4feb5f3149e98398cfe82fd1aba3067804e6ac9b
yasinehsan <yasinehsan11@gmail.com>
2019-11-26 20:21:40 -0500

    added node class

--- app/src/main/java/com/yasinehsan/java/Main.java
+++ app/src/main/java/com/yasinehsan/java/Main.java
@@ -2,11 +2,19 @@ package com.yasinehsan.java;
 
 public class Main {
 
+    public class Node{
+
+    }
+    
+    public void dfs(Node start){
+
+    }
 
 
 
     public static void main(String[] args){
         System.out.println("pri nting 123");
+
 
     }
 }
