d4ae120148b7ed0e1c92fb5c31a07b441af8cfdc
yasinehsan <yasinehsan11@gmail.com>
2019-11-28 14:01:38 +0000

    added fib 1 and 2

--- app/src/main/java/com/yasinehsan/java/Main.java
+++ app/src/main/java/com/yasinehsan/java/Main.java
@@ -2,19 +2,43 @@ package com.yasinehsan.java;
 
 public class Main {
 
-    public class Node{
 
+    public static int fib1(int n){
+        if(n <= 1)
+            return n;
+        System.out.println(n);
+        return fib1(n-2) + fib1(n-1);
     }
 
-    public void dfs(Node start){
+    public static int fib2(int n){
+        int[] dp = new int[1000];
+        dp[0] = 0;
+        dp[1] = 1;
 
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
+
+//    public static int fib3(int n){
+//        int base1 = 0, base2 = 1;
+//
+//        for(int i )
+//    }
+
+
+
+
 
 
 
     public static void main(String[] args){
-        System.out.println("pri nting 123");
+        System.out.println("printing 123");
 
+        int[] arr = {1,2,3,4,5,100,111};
+        System.out.println(fib2(9));
 
     }
 }
